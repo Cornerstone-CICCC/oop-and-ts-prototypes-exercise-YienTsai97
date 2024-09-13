ad7050c9515ecca03e0d8a32b0c4aaae6f234e15
Yi-En <m951235@gmail.com>
2024-09-13 16:52:40 -0700

    Update

--- exercise3.js
+++ exercise3.js
@@ -4,20 +4,60 @@
 // 2. Ensure the functionality remains the same after converting.
 
 
-class Person {
+/* class Person {
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
-  
+
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
-  
+
   greet() {
     console.log(`Hello, my name is ${this.getFullName()}.`);
   }
+} */
+
+
+
+function Person(firstName, lastName) {
+  this._firstName = firstName
+  this._lastName = lastName
+
+  /*   Object.defineProperty(this, "firstName", {
+      get: function () {
+        return this._firstName
+      },
+      set: function (newFirstName) {
+        this._firstName = newFirstName
+      }
+    })
+
+    Object.defineProperty(this, "lastName", {
+      get: function () {
+        return this._lastName
+      },
+      set: function (newLastName) {
+        this._lastName = newLastName
+      }
+    }) */
 }
+
+/* Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.firstName} ${this.lastName}.`);
+}
+ */
+
+
+Person.prototype.getFullName = function () {
+  return `${this._firstName} ${this._lastName}`
+}
+
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.getFullName()}.`);
+}
+
 
 
 // Test the Person constructor function
